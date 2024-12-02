9c2d4d57cb03f75d64a93a4f7c42638661520d3b
herguad <g.s.herrera.s@gmail.com>
2024-12-02 16:26:44 -0300

    initiate

---
+++ README.md
@@ -0,0 +1,1 @@
+Página web en proceso. Incluye estructura básica en HTML y estilos en css.
---
+++ js/script_gh.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 5;
+//             true  && true
+console.log( 10 == a && b > 0 );
+
+let x = false;
+let y = true;
+console.log( x || y );
+
+console.log( !x );
+console.log( !y );
+
+
