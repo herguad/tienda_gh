8ad37aca60bf583bdc59530e519ad5ff3c6f6c9f
herguad <g.s.herrera.s@gmail.com>
2024-12-12 18:42:01 -0300

    disc_disponible

--- js/script_gh.js
+++ js/script_gh.js
@@ -11,42 +11,42 @@ function processProductList(productos) {
            console.error(`${i} no es un nombre. Ingresá un nombre.`, product);
            return;
        };
-       if (typeof product.price !== 'number' || typeof product.descuento !== 'boolean') {
+       if (typeof product.price !== 'number' || typeof product.disponible !== 'boolean') {
          console.error(`${i} no es un número. Ingresá un número.`, product);
          return;
      }
-     if (typeof product.descuento !== 'boolean') {
+     if (typeof product.disponible !== 'boolean') {
       console.error(`${i} no es un valor True/False. Ingresá un valor True o False.`, product);
       return;
   }
    }
 
-   let totalConDesc = 0;
-   let totalSinDesc = 0;
+   let totalDispon = 0;
+   let totalNoDisp = 0;
 
-// Iterar sobre la lista y sumar la cantidad de productos con descuento.
+// Iterar sobre la lista y sumar la cantidad de productos con disponible.
    for (let i = 0; i < productos.length; i++) {
        const producto = productos[i];
-       if (producto.descuento === true) {
-           totalConDesc++;
+       if (producto.disponible === true) {
+           totalDispon++;
        } else {
-           totalSinDesc++;
+           totalNoDisp++;
        }
    }
 
    // Mostrar resultados en consola.
-   console.log(`Total productos con descuento: ${totalConDesc}`);
-   console.log(`Total productos sin descuento: ${totalSinDesc}`);
-   console.log(`Total productos: ${totalConDesc + totalSinDesc}`);
+   console.log(`Total productos con disponible: ${totalDispon}`);
+   console.log(`Total productos sin disponible: ${totalNoDisp}`);
+   console.log(`Total productos: ${totalDispon + totalNoDisp}`);
 }
 
 // Lista de ejemplo y cotejo de función.
 const productList = [
-   { name: "Producto 1", price: 400, descuento: true },
-   { name: "Producto 2", price: 200, descuento: false },
-   { name: "Producto 3", price: 150, descuento: true },
-   { name: "Producto 4", price: 350, descuento: true },
-   { name: "Producto 5", price: 300, descuento: false }
+   { name: "Producto 1", price: 400, disponible: true },
+   { name: "Producto 2", price: 200, disponible: false },
+   { name: "Producto 3", price: 150, disponible: true },
+   { name: "Producto 4", price: 350, disponible: true },
+   { name: "Producto 5", price: 300, disponible: false }
 ];
 
 processProductList(productList);
