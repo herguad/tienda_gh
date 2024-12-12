68ecc96ebcc7307cdd2582ef9f3061eec39931c3
herguad <g.s.herrera.s@gmail.com>
2024-12-12 18:27:28 -0300

    JS_listar_productos

--- js/script_gh.js
+++ js/script_gh.js
@@ -1,161 +1,47 @@
-let a = 10;
-let b = 5;
-//             true  && true
-console.log( 10 == a && b > 0 );
+// Procesar la lista de productos
+function processProductList(productos) {
+   // Validar los datos ingresados.
+   for (let i = 0; i < productos.length; i++) {
+       const product = productos[i];
+       if (typeof product.name !== 'string') {
+           console.error(`${i} no es un nombre. Ingresá un nombre.`, product);
+           return;
+       };
+       if (typeof product.price !== 'number' || typeof product.descuento !== 'boolean') {
+         console.error(`${i} no es un número. Ingresá un número.`, product);
+         return;
+     }
+     if (typeof product.descuento !== 'boolean') {
+      console.error(`${i} no es un valor True/False. Ingresá un valor True o False.`, product);
+      return;
+  }
+   }
 
+   let totalConDesc = 0;
+   let totalSinDesc = 0;
 
-console.log(a > 0);
-if ( a > 0 ) {
-   console.log(`El valor ${a} es mayor de 0`);
-};
+   // Iterar sobre la lista y sumar la cantidad de productos con descuento.
+   for (let i = 0; i < productos.length; i++) {
+       const producto = productos[i];
+       if (producto.descuento === true) {
+           totalConDesc++;
+       } else {
+           totalSinDesc++;
+       }
+   }
 
-a = -10;
-
-console.log(a > 0);
-if ( a > 0 ) {
-   console.log(`El valor ${a} es mayor de 0`);
-}
-else {
-   console.log(`El valor ${a} es menor de 0`);
-}
-
-// Sentencia if else if else if
-a = 0;
-
-console.log(a > 0);
-if ( a > 0 ) {
-   console.log(`El valor ${a} es mayor de 0`);
-}
-else if (a < 0) {
-   console.log(`El valor ${a} es menor de 0`);
-}
-else if ( a == 0) {
-   console.log(`El valor ${a} es igual a 0`);
-}
-
-// Otro tipo de condicional Switch
-let dia = 10;
-
-switch(dia) {
-   case 1:
-      console.log('Hoy es lunes');
-      break;
-   case 2:
-      console.log('Hoy es martes');
-      break;
-   case 3:
-      console.log('Hoy es miercoles');
-      break;
-   case 4:
-      console.log('Hoy es jueves');
-      break;
-   case 5:
-      console.log('Hoy es viernes');
-      break;
-   case 6:
-      console.log('Hoy es sabado');
-      break;
-   case 7:
-      console.log('Hoy es domingo');
-      break;
-   default:
-      console.log('No ingreso un día de la semana');
-      break;
-};
-
-//while
-let numero = 5, aux = 0;
-
-/* while ( aux != 0 ) {
-   console.log(numero);
-   aux = aux + 1;
-   aux += 1;
-   aux++;
-};
-*/
-
-let numerico, suma = 0;
-numerico = prompt('Ingrese un número distinto de 0');
-
-while(numerico != 0) {
-   numerico = parseFloat(prompt('Ingrese un número '));
-//console.log(typeof numerico);
-   suma = suma + numerico;
-   console.log("Aquí va el texto", numerico,suma);
-   console.log('Aquí va el texto'+ numerico + suma);
-   console.log(`Nuevo valor ingresado es ${numerico}`)
-}
-console.log(`Resultado de todos los números ingresados ${suma}`);
-
-let arreglo = [];
-
-arreglo[0] = 5;
-arreglo[3] = 8;
-arreglo[4] = 'palabra';
-
-console.log(`Elemento 1 [0]: ${arreglo[0]}`); 
-console.log(`Elemento 2 [1]: ${arreglo[1]}`); 
-console.log(`Elemento 3 [2]: ${arreglo[2]}`); 
-
-for(let aux=0; aux < arreglo.length; aux++) {
-   console.log(`Elemento [${aux}]: ${arreglo[aux]}`);   
-};
-
-//Funciones
-
-function sumar(parametro1, parametro2) {
-    let resultado;
-    resultado = parametro1 + parametro2;
- }
-
-alert('Ingresar valores para realizar una suma')
-for(let aux=0; aux < 1; aux++) {
-   a = Number(prompt("Ingresar valor"));
-   b = Number(prompt('Ingresar otro valor'));
-   sumar(a,b); //argumento
+   // Mostrar resultados en consola.
+   console.log(`Total productos con descuento: ${totalConDesc}`);
+   console.log(`Total productos sin descuento: ${totalSinDesc}`);
+   console.log(`Total productos: ${totalConDesc + totalSinDesc}`);
 }
 
-console.log(`El valor de la suma es ${aux}`);  
+// Lista de ejemplo y cotejo de función.
+const productList = [
+   { name: "Product 1", price: 100, descuento: true },
+   { name: "Product 2", price: 200, descuento: false },
+   { name: "Product 3", price: 150, descuento: true },
+   { name: "Product 4", price: 250, descuento: false },
+];
 
-function cambiarVariable(parametro) {
-    parametro = 20;
-    console.log(parametro);
- }
- 
- let argumento = 10;
- 
- cambiarVariable(argumento);
- 
- console.log(argumento);
- 
- //----------
- let variableGlobal = 'Saludo';
- 
- function cambiarValor(variableLocal) {
-    variableLocal = 'Hola';
-    variableGlobal = 'Nuevo Saludo';
- }
- 
- console.log(variableGlobal);
- 
- cambiarValor(variableGlobal);
- 
- console.log(variableGlobal);
- 
- // Operadores Ternarios
-//condicion? true : false
-let varA = 3;
-
-(varA == 1)? console.log('El valor es igual a 10') : console.log("El valor es diferente de 10");
-
-
-document.querySelector("html body h1");
-
-document.getElementById('ident');
-
-//devuelve un array de objetos
-document.getElementsByClassName('clase')[0];
-
-document.getElementsByName('nameH1')[0];
-
-document.getElementsByTagName('h1')[0];
+processProductList(productList);
